94aa5a7353c7dc28184533c5224dab9247c36f87
Lisa Takahashi <paris.tokyo.77777@gmail.com>
2021-12-08 08:29:11 +0900

    add kihonkyu for shunin

--- practice4/shunin.rb
+++ practice4/shunin.rb
@@ -5,5 +5,9 @@ class Shunin < Shain
   def standup
    puts '主任が素早く立ち上がりました'
   end
+
+  def caluculate_salary(kihonkyu)
+    kihonkyu*2+1
+  end
 end
 
--- practice4/tanto.rb
+++ practice4/tanto.rb
@@ -4,4 +4,9 @@ class Tanto < Shain
   def standup
     puts '担当は慌てて起立しました'
   end
+
+
+ def caluculate_salary(kihonkyu)
+  kihonkyu
+ end
 end
