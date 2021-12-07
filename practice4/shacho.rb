0fa970322100d4c78520835aab555adfed05d891
Lisa Takahashi <paris.tokyo.77777@gmail.com>
2021-12-07 17:50:38 +0900

    set shacho.rb

---
+++ practice4/shacho.rb
@@ -0,0 +1,13 @@
+require_relative 'shain'
+require_relative 'tanto'
+require_relative 'shunin'
+require_relative 'bucho'
+
+shain = Tanto.new
+
+shain.standup
+
+shain = Shunin.new
+
+bucho = Bucho.new
+
