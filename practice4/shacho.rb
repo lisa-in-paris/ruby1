af28eff5d68853a20dea00dcdbd304741185624c
Lisa Takahashi <paris.tokyo.77777@gmail.com>
2021-12-08 15:27:23 +0900

    set shacho.rb

--- practice4/shacho.rb
+++ practice4/shacho.rb
@@ -4,10 +4,14 @@ require_relative 'shunin'
 require_relative 'bucho'
 
 shain = Tanto.new
-
 shain.standup
+puts '私の給料は#{shain.calculate_salary(100)}円です。'
 
 shain = Shunin.new
+shain.standup
+puts '私の給料は#{shain.calculate_salary(100)}です。'
 
 bucho = Bucho.new
+shain.standup
+puts '私の給料は#{shain.calculate_salary(100)}です。'
 
