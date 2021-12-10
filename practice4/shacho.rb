5fcb3202793eaf102132a235cc7ad40929848af7
Lisa Takahashi <paris.tokyo.77777@gmail.com>
2021-12-10 13:56:29 +0900

    add senmu into shacho.rb

--- practice4/shacho.rb
+++ practice4/shacho.rb
@@ -2,6 +2,7 @@ require_relative 'shain'
 require_relative 'tanto'
 require_relative 'shunin'
 require_relative 'bucho'
+require_relative 'senmu'
 
 shain = Tanto.new
 shain.standup
@@ -12,6 +13,10 @@ shain.standup
 puts '私の給料は#{shain.calculate_salary(100)}です。'
 
 bucho = Bucho.new
+shain.standup
+puts '私の給料は#{shain.calculate_salary(100)}です。'
+
+senmu = Senmu.new
 shain.standup
 puts '私の給料は#{shain.calculate_salary(100)}です。'
 
