3d614a5db9c841cabb7fc87cd8e1e032a7af1d8e
Lisa Takahashi <paris.tokyo.77777@gmail.com>
2021-12-10 13:51:26 +0900

    fix Rspec for senmu

--- practice4/_senmu_spec.rb
+++ practice4/_senmu_spec.rb
@@ -3,7 +3,7 @@ require_relative 'senmu'
 descrive Senmu do
   let(:senmu) { Senmu.new }
 
-  example '担当の給料は基本給と一緒。基本給が100なら給料も100' do
+  example '専務の給料は基本給の４倍＋２。基本給が100なら給料は402' do
     expect(senmu.calculate_salary(100)).to eq 100
   end
 end
