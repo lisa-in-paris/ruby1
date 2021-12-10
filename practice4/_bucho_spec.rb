51136282b5b6fb9cce1eb53aacd6e4be8b5b4587
Lisa Takahashi <paris.tokyo.77777@gmail.com>
2021-12-10 13:50:32 +0900

    set Rspec for bucho

---
+++ practice4/_bucho_spec.rb
@@ -0,0 +1,10 @@
+require_relative 'bucho'
+
+descrive Bucho do
+  let(:bucho) { Bucho.new }
+
+  example '部長の給料の３倍。基本給が100なら給料も300' do
+    expect(bucho.calculate_salary(100)).to eq 100
+  end
+  
+end
