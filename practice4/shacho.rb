cc8053edb0996da5afc5d207994adcedace14395
Lisa Takahashi <paris.tokyo.77777@gmail.com>
2021-12-13 20:47:41 +0900

    modify  refactoring for shacho.rb

--- practice4/shacho.rb
+++ practice4/shacho.rb
@@ -2,19 +2,26 @@ require_relative 'shain_factory'
 
 shain_factory = ShainFactory.new
 
-shain = shain_factory.create('Tanto',100)
-shain.standup
-puts '私の給料は#{shain.calculate_salary(100)}円です。'
+['Tanto', 'Shunin', 'Bucho', 'Senmu'].each do |yakushoku|
+  shain =shain_factory.create(yakusoku, 100)
+  puts shain.standup
+  puts '私の給料は#{shain.caluculate_salary} yenです。'
+end
 
-shain = shain_factory.create('Shunin',100)
-shain.standup
-puts '私の給料は#{shain.calculate_salary(100)}です。'
 
-bucho = shain_factory.create('Bucho',100)
-shain.standup
-puts '私の給料は#{shain.calculate_salary(100)}です。'
+# shain = shain_factory.create('Tanto',100)
+# shain.standup
+# puts '私の給料は#{shain.calculate_salary(100)}円です。'
 
-senmu = shain_factory.create('Senmu',100)
-shain.standup
-puts '私の給料は#{shain.calculate_salary(100)}です。'
+# shain = shain_factory.create('Shunin',100)
+# shain.standup
+# puts '私の給料は#{shain.calculate_salary(100)}です。'
+
+# bucho = shain_factory.create('Bucho',100)
+# shain.standup
+# puts '私の給料は#{shain.calculate_salary(100)}です。'
+
+# senmu = shain_factory.create('Senmu',100)
+# shain.standup
+# puts '私の給料は#{shain.calculate_salary(100)}です。'
 
